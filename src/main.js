e0e3a6a6974ff93edf79de0961c6115015f0a29f
cc <sheri_situ@163.com>
2020-12-22 14:16:46 +0800

    feat: storage封装和接口拦截

--- src/main.js
+++ src/main.js
@@ -1,8 +1,26 @@
 import Vue from 'vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// 根据前端的跨域方式做调整
+axios.defaults.baseURL = '/api'
+axios.defaults.timeout = 8000
+// 请求拦截
+axios.interceptors.response.use(function (response) {
+  const res = response.data
+  if (res.status === 0) {
+    return res.data
+  } else if (res.status === 10) { // 未登录
+    window.location.href = '/#/login'
+  } else {
+    alert(res.msg)
+  }
+})
+
+Vue.use(VueAxios, axios)
 Vue.config.productionTip = false
 
 new Vue({
---
+++ src/storage/index.js
@@ -0,0 +1,38 @@
+/**
+ *  storage 封装
+ * **/
+const STORAGE_KEY = 'mall'
+
+export default {
+  setItem (key, value, moduleName) {
+    if (moduleName) {
+      const val = this.getItem(moduleName)
+      val[key] = value
+      this.setItem(moduleName, val)
+    } else {
+      const val = this.getStorage()
+      val[key] = value
+      window.sessionStorage.setItem(STORAGE_KEY, val)
+    }
+  },
+  // 获取某一个模块下面的属性，如{user: {userName: {}}}
+  getItem (key, moduleName) {
+    if (moduleName) {
+      const val = this.getItem(moduleName)
+      if (val) return val[key]
+    }
+    return this.getStorage()[key]
+  },
+  getStorage () {
+    return JSON.parse(window.sessionStorage.getItem(STORAGE_KEY) || '{}')
+  },
+  clear (key, moduleName) {
+    const val = this.getStorage()
+    if (moduleName) {
+      delete val[moduleName][key]
+    } else {
+      delete val[key]
+    }
+    window.sessionStorage.setItem(STORAGE_KEY, val)
+  }
+}
